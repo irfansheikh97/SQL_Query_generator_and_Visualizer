2922dc347710ac6b3e97d2cea9321093f59ca222
Irfan Sheikh <43146373+irfansheikh97@users.noreply.github.com>
2024-09-18 11:53:45 +0530

    Add files via upload

---
+++ model_methods.py
@@ -0,0 +1,49 @@
+import sqlite3
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import google.generativeai as genai
+
+
+def check_api_key(api_key: str) -> None:
+    '''
+    Function to check whether the API key was really exist.
+    '''
+    if len(api_key) != 0:
+        try:
+            genai.configure(api_key=api_key)
+            model = genai.GenerativeModel('gemini-pro')
+            response = model.generate_content("Hello")
+            st.write("Gemini API key is valid!")
+        except Exception as e:
+            st.warning(e)
+
+# Function to get response from the Generative Model API
+def get_response(question, prompt):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content([prompt[0], question])
+    return response.text
+
+
+# Function to execute SQL query and return results as DataFrame
+def read_sql_query(sql, db):
+    conn = sqlite3.connect(db)
+    df = pd.read_sql_query(sql, conn)
+    conn.close()
+    return df
+
+
+# Function to plot data
+def plot_data(df, chart_type, x_col, y_col):
+    if chart_type == 'Bar Chart':
+        fig = px.bar(df, x=x_col, y=y_col)
+    elif chart_type == 'Line Chart':
+        fig = px.line(df, x=x_col, y=y_col)
+    elif chart_type == 'Pie Chart':
+        fig = px.pie(df, names=x_col, values=y_col)
+    elif chart_type == 'Scatter Chart':
+        fig = px.scatter(df, x=x_col, y=y_col)
+    else:
+        st.error("Unsupported chart type")
+        return None
+    return fig
